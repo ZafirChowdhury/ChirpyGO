5b0ddca3575a4dbf2c4f02888ff8e784ad704ce0
zafir <zafirchowdhury69@gmail.com>
2026-05-20 08:38:22 +0600

    Custom Handlers

--- main.go
+++ main.go
@@ -10,7 +10,11 @@ func main() {
 	const rootDir = "."
 
 	serverMux := http.NewServeMux()
-	serverMux.Handle("/", http.FileServer(http.Dir(rootDir)))
+
+	// file server
+	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(rootDir))))
+
+	serverMux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -19,4 +23,10 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
